674311ec972687fc6516e77c31e029618d6e1bac
AmanT2817 <amantade1996@gmail.com>
2026-05-30 17:30:39 +0300

    Simplify DB connection

--- backend/app/utils/helpers.py
+++ backend/app/utils/helpers.py
@@ -2,24 +2,24 @@ import pymysql
 from flask import current_app
 
 def get_db():
-    conn_params = {
-        "host": current_app.config['MYSQL_HOST'],
-        "user": current_app.config['MYSQL_USER'],
-        "password": current_app.config['MYSQL_PASSWORD'],
-        "database": current_app.config['MYSQL_DB'],
-        "port": current_app.config['MYSQL_PORT'],
-        "cursorclass": pymysql.cursors.DictCursor,
-        "connect_timeout": 10,
-        "read_timeout": 10,
-        "write_timeout": 10
-    }
-    
-    if current_app.config.get('MYSQL_SSL', False):
-        conn_params['ssl_disabled'] = False
-        conn_params['ssl_verify_cert'] = False
-        conn_params['ssl_verify_identity'] = False
-    
-    conn = pymysql.connect(**conn_params)
+    host = current_app.config['MYSQL_HOST']
+    port = current_app.config['MYSQL_PORT']
+    user = current_app.config['MYSQL_USER']
+    password = current_app.config['MYSQL_PASSWORD']
+    database = current_app.config['MYSQL_DB']
+
+    conn = pymysql.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database,
+        port=int(port),
+        cursorclass=pymysql.cursors.DictCursor,
+        connect_timeout=10,
+        read_timeout=10,
+        write_timeout=10,
+        ssl_disabled=True
+    )
     return conn
 
 def success_response(data=None, message="Success", status=200):
